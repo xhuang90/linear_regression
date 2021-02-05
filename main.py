8ceaf4948c9556755dca4c2e07bf5b420b4cebb7
xhuang90 <huangxinhong@cvte.com>
2021-02-05 17:59:53 +0800

    add: plots

--- main.py
+++ main.py
@@ -5,12 +5,22 @@
 # @Desc    :
 
 
-from data_preprocess import create_new_dataset
+from modeling import create_new_dataset
 
 
 def create_new_data():
+    """
+        Start the experiment by creating 3 additional training les from the train-1000-100.csv
+        by taking the first 50, 100, and 150 instances respectively.
+        Call them: train-50(1000)-100.csv, train-100(1000)-100.csv, train-150(1000)-100.csv.
+        The corresponding test file for
+        these dataset would be test-1000-100.csv and no modification is needed.
+    @return:
+    @rtype:
+    """
 
     for num in [50, 100, 150]:
+
         create_new_dataset(num_of_data=num)
 
 
--- modeling.py
+++ modeling.py
@@ -9,13 +9,30 @@ https://www.cnblogs.com/orangecyh/p/11647825.html
 
 Implement L2 regularized linear regression algorithm with  ranging from 0 to 150
 (integers only). For each of the 6 dataset, plot both the training set MSE and the test
-set MSE as a function of  (x-axis) in one graph.
+set MSE as a function of (x-axis) in one graph.
 """
 
 import pandas as pd
 import numpy as np
 from logs import logger
 import matplotlib.pyplot as plt
+
+
+def create_new_dataset(num_of_data):
+    """
+
+    @param num_of_data:
+    @type num_of_data: int
+    @return: None
+    @rtype: None
+    """
+
+    df_train = pd.read_csv('data/train-1000-100.csv')
+    df_train_sub = df_train.head(num_of_data)
+
+    # save new dataset
+    file_out_name = 'train-{}(1000)-100'.format(num_of_data)
+    df_train_sub.to_csv('data/{}.csv'.format(file_out_name), index=False)
 
 
 def construct_x_y_matrix(file_name):
@@ -101,7 +118,7 @@ def calculate_trn_test_mse(trn_file_name, test_file_name):
         weight = calculate_beta(train_x, train_y, coefficient=theta)
         train_y_pred = np.dot(train_x, weight)
         train_mse = calculate_mse(y_label=train_y, y_pred=train_y_pred)
-
+        # test
         test_y_pred = np.dot(test_x, weight)
         test_mse = calculate_mse(y_label=test_y, y_pred=test_y_pred)
 
@@ -134,6 +151,32 @@ def get_min_test_mse(trn_file_name, test_file_name):
                 .format(trn_file_name, test_file_name, theta_, test_mse_))
 
 
+def plot_theta_mse(trn_file_name, test_file_name):
+    """
+
+    @param trn_file_name:
+    @type trn_file_name:
+    @param test_file_name:
+    @type test_file_name:
+    @return:
+    @rtype:
+    """
+    # get train MSE and test MSE with different theta
+    train_mse_dict, test_mse_dict = calculate_trn_test_mse(trn_file_name, test_file_name)
+    # construct x axis
+    theta_val = [i for i in range(0, 151)]
+    # construct y axis
+    y_train_mse = list(train_mse_dict.values())
+    y_test_mse = list(test_mse_dict.values())
+    # plot theta and MSE
+    plt.plot(theta_val, y_train_mse)
+    plt.plot(theta_val, y_test_mse)
+    # then save image
+    pic_name = 'tmp_res/{}_{}_theta_mse'.format(trn_file_name, test_file_name)
+    plt.savefig(pic_name + '.png')
+    logger.info('{} is saved!'.format(pic_name))
+
+
 def calculate_loss_function(x_arr, y, coefficient):
     """
 
@@ -152,17 +195,10 @@ def calculate_loss_function(x_arr, y, coefficient):
 
 if __name__ == '__main__':
 
-    # read_dataset(file_name='train-100-10')
     train_dataset = ['train-100-10', 'train-100-100', 'train-1000-100',
                      'train-50(1000)-100', 'train-100(1000)-100', 'train-150(1000)-100']
     test_dataset = ['test-100-10', 'test-100-100', 'test-1000-100',
                     'test-1000-100', 'test-1000-100', 'test-1000-100']
     for trn, test in zip(train_dataset, test_dataset):
-        get_min_test_mse(trn_file_name=trn, test_file_name=test)
-
-    # print(y_label)
-    # print(train_x.dot(weight))
-    # read_dataset(file_name='train-1000-100')
-    # read_dataset(file_name='train-50(1000)-100')
-    # read_dataset(file_name='train-100(1000)-100')
-    # read_dataset(file_name='train-150(1000)-100')
+        # get_min_test_mse(trn_file_name=trn, test_file_name=test)
+        plot_theta_mse(trn_file_name=trn, test_file_name=test)
